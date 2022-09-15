88b08a322c6318714cd0c8c5e7fd69991aa34441
julianBrandon <p0lterg3istt@gmail.com>
2022-09-15 15:12:47 +0200

    Implement functionality to display and move player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,69 @@
-console.log("loaded");
+
+class Game {
+    constructor(){
+        this.player = null; //will store an instance of the class Player
+    }
+    start(){
+        this.player = new Player();
+        this.attachEventListeners();
+    }
+    attachEventListeners(){
+        document.addEventListener("keydown", (event) => {
+            if(event.key === "ArrowLeft"){
+                console.log("move player to the left")
+                this.player.moveLeft();
+            }else if(event.key === "ArrowRight"){
+                console.log("move player to the right")
+                this.player.moveRight();
+            }
+        });
+    }
+}
+
+
+class Player {
+    constructor(){
+        this.positionX = 40;
+        this.positionY = 0;
+        this.width = 20;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
+
+        // set id and css
+        this.domElement.id = "player";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        // append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+    }
+    moveLeft(){
+        this.positionX--;
+        const newDiv = document.getElementById("player");
+        this.domElement.style.left = this.positionX + "vw";
+        
+    }
+    moveRight(){
+        console.log("moving right....");
+        this.positionX++;
+        const newDiv = document.getElementById("player");
+        this.domElement.style.left = this.positionX + "vw";
+    }
+}
+
+
+
+const game = new Game();
+game.start();
+
+
